d3ea8824db303c8631eca1c4ed103b5402501593
João Artur Magalhães <joaoartur4590@gmail.com>
2023-01-14 20:55:56 -0300

    finalizado1

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () =>
-  console.log("server rodando na porta 3333")
+  console.log("server rodando na porta 3333" + process.env.PORT)
 );
